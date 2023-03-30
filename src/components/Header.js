ac17d424d5a802e82e2e2392a103e4abf206fd2c
Mohit Gawande <mohit.gawande65@gmail.com>
2023-03-30 13:02:37 +0530

    added ref to get access  of instant value of input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { onSubmit } from '../Redux/ActionCreator'
-export default function Header({onClickCity}) {
+export default function Header({ onClickCity }) {
 
     const dispatch = useDispatch()
-    const [inputCity, setInputCity] = useState('')
-
+    const inputCity = useRef('')
     return (
         <div>
             <div className='bg-secondary d-flex flex-wrap justify-content-center align-items-center p-1 w-100'>
-                <h6 className='my-1 ml-3 text-white flex-grow-1 '>Weather App</h6>                <input onChange={(e) => { setInputCity(e.target.value); dispatch(onSubmit(inputCity)) }} value={inputCity} placeholder='Enter city name' />
+                <h6 className='my-1 ml-3 text-white flex-grow-1 '>Weather App</h6>
+                <input onChange={(e) => { dispatch(onSubmit(inputCity.current.value)) }} ref={inputCity} placeholder='Enter city name' />
                 <div className=''>
                     <button onClick={onClickCity} type="submit" className="btn btn-success my-1 mx-2 rounded-0">Submit</button>
                 </div>
