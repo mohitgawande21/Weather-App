002d3971c2a7c6f26289742fc7616c6c68abefa7
Mohit Gawande <mohit.gawande65@gmail.com>
2023-04-03 19:17:03 +0530

    added info color

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -7,10 +7,10 @@ export default function WeatherCard({ inputCityName, url, cityRes }) {
     return <>
         <h5  className="card-body text-center ">Enter city name and submit to see the change of bellow card with live weather</h5>
         <div className="hoverCard card w-25 m-auto my-3 " >
-            <Link className=' text-decoration-none text-info' to={`/${inputCityName}`}>
+            <Link className=' text-decoration-none ' to={`/${inputCityName}`}>
                 <div className="card-body text-center ">
                     <h3 className='card-title '>{inputCityName}</h3>
-                    <div className="hover::.bg-red card-text">{new Date().toLocaleString('en-us', { weekday: 'long' })}</div>
+                    <div className="card-text">{new Date().toLocaleString('en-us', { weekday: 'long' })}</div>
                     <img className="card-img-top" src={url} />
                     <p className="card-text">{cityRes?.main?.temp} °C</p>
                     <p className="card-text">{cityRes?.main?.temp ? cityRes.weather[0].description : ''}</p>
